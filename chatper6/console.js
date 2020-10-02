56b0de08f8447e02889760d5bdc0f0584f331ff2
izonejangwonyoung <2000shim@naver.com>
2020-10-02 15:53:13 +0900

    chapter6

---
+++ chatper6/console.js
@@ -0,0 +1,13 @@
+console.error("바보")
+console.time()
+console.timeEnd()
+
+console.info("ff")
+
+
+console.time("answer_time")
+alert("확인 버튼을 누르십시오")
+console.timeEnd("answer_time")
+
+
+///chrome console에서  실 행 해 볼 것
